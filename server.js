d181bd0b592b382f826931140e64462a968ef487
sureshcerebral <r_s_k@hotmail.com>
2016-11-17 21:21:27 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -12,10 +12,10 @@ app.get('/articleone', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'articleone.html'));
 });
 app.get('/articletwo', function (req, res) {
-  res.send('Article two will be added shortly');
+  res.sendFile(path.join(__dirname, 'ui', 'articletwo.html'));
 });
 app.get('/articlethree', function (req, res) {
-  res.send('Article three will be added shortly');
+  res.sendFile(path.join(__dirname, 'ui', 'articlethree.html'));
 });
 
 
