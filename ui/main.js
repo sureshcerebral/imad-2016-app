eaf30cec55dd0bf0dcca31e56d853fb3a26ba764
sureshcerebral <r_s_k@hotmail.com>
2016-11-18 21:40:23 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,3 +1,9 @@
 console.log('Loaded!');
 var element=document.getElementById('main-text');
 element.innerHTML = "Modified Text to New value";
+
+var img = document.getElementById("logo");
+
+img.onclick = function(){
+    img.style.marginLeft="100px";
+};
