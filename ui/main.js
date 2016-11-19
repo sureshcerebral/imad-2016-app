3bd91b621804ed60c31d72ba46f828b750152bfc
sureshcerebral <r_s_k@hotmail.com>
2016-11-19 19:47:45 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,17 +1,27 @@
-console.log('Loaded!');
-var element=document.getElementById('main-text');
-element.innerHTML = "Modified Text to New value";
+//code for counter end point
+var button = document.getElementById('counter');
 
-var img = document.getElementById("logo");
+button.onclick = function(){
+  
+  // make a request object
+  var request = new XMLHttpRequest();
+  
+  // capture the response into a var
+  request.onreadystatechange = function (){
+     if (request.readyState === XMLHttpRequest.Done) // httprequest is completed
+     {
+         if (request.status == 200)
+         {
+              var counter = request.responseText;
+              var span = document.getElementById('count');
+              span.innerHTML = counter.toString();
+         }
+     }
+     // else means do nothing and wait for it to complete..
+      
+  };
+  // make the actual request to the web page
+  request.open('GET','http://sureshcerebral.imad.hasura-app.io/counter',true);
+  request.send(null);
 
-var marginLeft = 0;
-function moveRight(){
-  marginLeft = marginLeft+1;
-  img.style.marginLeft = marginLeft+'px';
-}
-
-img.onclick = function(){
-    
-    var interval = setInterval(moveRight,50);
-    //img.style.marginLeft="100px";
 };
