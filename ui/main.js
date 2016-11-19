56ecce7b444d68864164806b725e7b27114600b1
sureshcerebral <r_s_k@hotmail.com>
2016-11-19 21:42:55 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -25,3 +25,23 @@ button.onclick = function(){
   request.send(null);
 
 };
+
+// name input display
+var nameInput = document.getElementById('name');
+var name = nameInput.value;
+var submit = document.getElementById('submit_btn');
+
+submit.onclick = function(){
+  
+  // make a server request and send the name
+  
+  //capture a list of the names and render it as list
+  var names = ['name1','name2','name3'];
+  var list = '';
+  for (var i=0;i<names.length;i++){
+    list += '<li>' + names[i] + '</li>';     
+  }
+  var sl = document.getElementById('nameList');
+  sl.innerHTML = list;
+  
+};
