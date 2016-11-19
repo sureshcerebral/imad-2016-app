c753c35be72ef2bda1faa1f147353ea28a97a94f
sureshcerebral <r_s_k@hotmail.com>
2016-11-19 22:07:04 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -110,10 +110,15 @@ app.get('/ui/madi.png', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'madi.png'));
 });
 
-app.get('/ui/cerebral_logo.jpg', function (req, res) {
-  res.sendFile(path.join(__dirname, 'ui', 'cerebral_logo.jpg'));
+var names=[];
+app.get('/submit-name/:name', function (req, res) {
+    // get the name from request
+    
+    var name = req.params.name;
+    names.push(name);
+    //JSON .. Javascript Object Notation
+    res.send(JSON.stringify(names));
 });
-
 var port = 8080; // Use 8080 for local development because you might already have apache running on 80
 app.listen(8080, function () {
   console.log(`IMAD course app listening on port ${port}!`);
