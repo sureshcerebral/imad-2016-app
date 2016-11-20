870081c0967668a815729225d6998bb8eae425be
sureshcerebral <r_s_k@hotmail.com>
2016-11-20 09:35:49 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -90,6 +90,16 @@ app.get('/counter', function (req, res) {
   res.send(counter.toString());
 });
 
+var names=[];
+app.get('/submit-name/:name', function (req, res) {
+    // get the name from request
+    
+    var name = req.params.name;
+    names.push(name);
+    //JSON .. Javascript Object Notation
+    res.send(JSON.stringify(names));
+});
+
 app.get('/:articleName', function (req, res) {
     //articleName refers to the object names like articleOne etc..
     // eg - articleName == articleOne;
@@ -112,15 +122,6 @@ app.get('/ui/cerebral_logo.jpg', function (req, res) {
 });
 
 
-var names=[];
-app.get('/submit-name/:name', function (req, res) {
-    // get the name from request
-    
-    var name = req.params.name;
-    names.push(name);
-    //JSON .. Javascript Object Notation
-    res.send(JSON.stringify(names));
-});
 var port = 8080; // Use 8080 for local development because you might already have apache running on 80
 app.listen(8080, function () {
   console.log(`IMAD course app listening on port ${port}!`);
