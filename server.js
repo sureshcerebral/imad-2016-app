cc452491fb2dfba60dc789b20a1a3b661b65f0ce
sureshcerebral <r_s_k@hotmail.com>
2016-11-23 21:46:22 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -131,7 +131,7 @@ app.get('/articles/:articleName', function (req, res) {
     // eg - articleName == articleOne;
     //articles[articleName] = {} gives the content of articleOne object  
 
-  pool.query("SELECT * FROM article WHERE title='+req.params.articleName+'",function(err,result){
+  pool.query("SELECT * FROM article WHERE title='"+req.params.articleName+"'",function(err,result){
       if (err){
         res.status(500).send(err.toString());    
       }
