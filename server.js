74753a64d743559253f57b79e624aa4c7778c523
sureshcerebral <r_s_k@hotmail.com>
2016-11-24 13:17:48 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -64,7 +64,7 @@ app.get('/', function (req, res) {
 function hash(input,salt){
     
     var hashed = crypto.pbkdf2Sync(input, salt, 10000, 512, 'sha512');
-    return hashed.toString('hex');
+    return ["pbkdf2sync","10000",salt, hashed.toString('hex')].join('$');
 }
 
 app.get('/hash/:input',function(req,res){
